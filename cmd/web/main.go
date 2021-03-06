30a5759d012dc2656db58b3f0f8bd9d71198ddb1
owenburton <owen.burton13@gmail.com>
2021-03-06 13:32:13 -0700

    added caching for templates

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"database/sql"
 	"flag"
+	"html/template"
 	"log"
 	"net/http"
 	"os"
@@ -12,12 +13,11 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
-// Add a snippets field to the application struct. This will allow us to
-// make the SnippetModel object available to our handlers.
 type application struct {
-	errorLog *log.Logger
-	infoLog  *log.Logger
-	snippets *mysql.SnippetModel
+	errorLog      *log.Logger
+	infoLog       *log.Logger
+	snippets      *mysql.SnippetModel
+	templateCache map[string]*template.Template
 }
 
 func main() {
@@ -31,29 +31,27 @@ func main() {
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
-	// To keep the main() function tidy I've put the code for creating a connection
-	// pool into the separate openDB() function below. We pass openDB() the DSN
-	// from the command-line flag.
 	db, err := openDB(*dsn)
 	if err != nil {
 		errorLog.Fatal(err)
 	}
 
-	// We also defer a call to db.Close(), so that the connection pool is closed
-	// before the main() function exits.
 	defer db.Close()
 
-	// Initialize a new instance of application containing the dependencies.
-	app := &application{
-		errorLog: errorLog,
-		infoLog:  infoLog,
-		snippets: &mysql.SnippetModel{DB: db},
+	// Initialize a new template cache...
+	templateCache, err := newTemplateCache("./ui/html")
+	if err != nil {
+		errorLog.Fatal(err)
 	}
 
-	// Initialize a new http.Server struct. We set the Addr and Handler fields so
-	// that the server uses the same network address and routes as before, and set
-	// the ErrorLog field so that the server now uses the custom errorLog logger in
-	// the event of any problems.
+	// And add it to the application dependencies.
+	app := &application{
+		errorLog:      errorLog,
+		infoLog:       infoLog,
+		snippets:      &mysql.SnippetModel{DB: db},
+		templateCache: templateCache,
+	}
+
 	srv := &http.Server{
 		Addr:     *addr,
 		ErrorLog: errorLog,
@@ -61,9 +59,6 @@ func main() {
 	}
 
 	infoLog.Printf("Starting server on %s", *addr)
-	// Because the err variable is now already declared in the code above, we need
-	// to use the assignment operator = here, instead of the := 'declare and assign'
-	// operator.
 	err = srv.ListenAndServe()
 	errorLog.Fatal(err)
 }
--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -1,9 +1,55 @@
 package main
 
-import "owenburton.dev/snippetbox/pkg/models"
+import (
+	"html/template"
+	"path/filepath"
+
+	"owenburton.dev/snippetbox/pkg/models"
+)
 
 // Include a Snippets field in the templateData struct.
 type templateData struct {
 	Snippet  *models.Snippet
 	Snippets []*models.Snippet
+}
+
+func newTemplateCache(dir string) (map[string]*template.Template, error) {
+	// Initialize a new map to act as the cache.
+	cache := map[string]*template.Template{}
+
+	// Use the filepath.Glob function to get a slice of all filepaths with
+	// the extension '.page.tmpl'. This essentially gives us a slice of all the
+	// 'page' templates for the application.
+	pages, err := filepath.Glob(filepath.Join(dir, "*.page.tmpl"))
+	if err != nil {
+		return nil, err
+	}
+
+	// Loop through the pages one-by-one.
+	for _, page := range pages {
+		// Extract the file name (like 'home.page.tmpl') from the full file path
+		// and assign it to the name variable.
+		name := filepath.Base(page)
+
+		// Parse the page template file in to a template set.
+		ts, err := template.parseFiles(page)
+		if err != nil {
+			return nil, err
+		}
+
+		// Use the ParseGlob method to add any 'layout' templates to the
+		// template set (in our case, it's just the 'base' layout at the
+		// moment).
+		ts, err = ts.ParseGlob(filepath.Join(dir, "*.partial.tmpl"))
+		if err != nil {
+			return nil, err
+		}
+
+		// Add the template set to the cache, using the name of the page
+		// (like 'home.page.tmpl') as the key.
+		cache[name] = ts
+	}
+
+	// Return the map.
+	return cache, nil
 }
